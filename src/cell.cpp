1885e68f93ceb888bde22e519092063ce220a949
Maxwell Cole <103389732+EatYoCharms9@users.noreply.github.com>
2023-12-10 12:28:19 -0800

    Update cell.cpp, added functionality for collision detection

--- src/cell.cpp
+++ src/cell.cpp
@@ -4,9 +4,19 @@
 
 using namespace godot;
 
-void Cell::_bind_methods() {}
+void Cell::_bind_methods() {
+    ClassDB::bind_method(D_METHOD("_on_body_entered", "body"), &Cell::_on_body_entered);
+}
+
+int Cell::CollisionCount = 0; 
 
 Cell::Cell() {
+
+    // Setting default required parameters for collision detection on RigidBody2D objects, then applying the signal to Cell objects.
+    this->set_contact_monitor(true);
+    this->set_max_contacts_reported(1000); // Adjust max contacts as complexity increases.
+    this->connect("body_entered", Callable(this, "_on_body_entered"));
+
     rand.instantiate();
     force_magnitude = rand->randf_range(50.0, 150.0);
 
@@ -16,6 +26,7 @@ Cell::Cell() {
 }
 Cell::~Cell() {}
 
+
 void Cell::_process(double delta) {
     // Don't run if in editor
     if (Engine::get_singleton()->is_editor_hint())
@@ -24,4 +35,9 @@ void Cell::_process(double delta) {
     // float direction = rand->randf_range(0, 2 * Math_PI);
     // Vector2 force = Vector2(0, -1).rotated(direction) * speed;
     // apply_force(force);
+}
+
+// function updates on cell contacts. Increments counter for use in stats_counter.cpp
+void Cell::_on_body_entered(Node *body) {
+    CollisionCount++;
 }
