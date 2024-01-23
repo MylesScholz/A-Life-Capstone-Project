f7724bc188247431dad2217f94b788c0a77d1942
hpfluke0478 <38867454+hpfluke0478@users.noreply.github.com>
2024-01-23 00:50:59 +0000

    #39: Implemented Flagella class with movement force vector, began binding to CellState

--- src/cell_state.cpp
+++ src/cell_state.cpp
@@ -15,6 +15,7 @@ CellState::CellState() {
 	_age = 0;
 	_lifespan = 1;
 	_scale = 1;
+	_flagella = Flagella;
 }
 CellState::~CellState() {}
 
@@ -45,3 +46,34 @@ void CellState::applyScale(const float scale) {
 		_scale *= scale;
 }
 float CellState::getScale() const { return _scale; }
+
+void CellState::setMovementForce(const Vector2 &force) {
+	if (_flagella) {
+		_flagella = setMovementForce(force)
+	}
+}
+
+void CellState::getMovementForce() const {
+	return _flagella ? _flagella->getMovementForce() : Vector2():
+}
+
+void Flagella:_bind_methods() {
+	ClassDB::bind_method(D_METHOD("set_movement_force", "force"),
+			&Flagella::setMovementForce);
+	ClassDB::bind_method(D_METHOD("get_movement_force", &Flagella:getMovementForce));
+	ClassDB::add_property("Flagella", PropertyInfo(Variant::VECTOR2, "movement_force"),
+			"set_movement_force", "get_lifespan");
+}
+
+Flagella::Flagella() {
+	_movement_force = Vector2();
+}
+Flagella::~Flagella() {}
+
+void Flagella::setMovementForce(const Vector2 &force) {
+	_movement_force = force;
+}
+
+Vector2 Flagella::getMovementForce() const {
+	return _movement_force;
+}
--- src/cell_state.hpp
+++ src/cell_state.hpp
@@ -28,11 +28,33 @@ public:
 	void applyScale(const float);
 	float getScale() const;
 
+	void setMovementForce(const Vector2 &force);
+	Vector2 getMovementForce() const;
+
 private:
 	bool _alive;
 	float _age;
 	float _lifespan;
 	float _scale;
+	Ref<Flagella> _flagella;
+};
+
+class Flagella : public Node {
+	GDCLASS(Flagella, Node);
+
+protected:
+	static void _bind_methods():
+
+public:
+	Flagella():
+	~Flagella():
+
+	void setMovementForce(const Vector2 &force);
+	Vector2 getMovementForce() const;
+
+private:
+	Vector2 _movement_force;
+
 };
 
 }; // namespace godot
