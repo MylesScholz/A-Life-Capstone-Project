d83ad0e5a318750b9ba567a47576c4444a832f09
Alex-x90 <sashanrosenthal@gmail.com>
2024-02-01 16:47:32 -0800

    Another capitalization fix

--- src/cell.hpp
+++ src/cell.hpp
@@ -3,7 +3,7 @@
 
 #include <godot_cpp/classes/engine.hpp>
 #include <godot_cpp/classes/random_number_generator.hpp>
-#include <godot_cpp/classes/rigid_body2D.hpp>
+#include <godot_cpp/classes/rigid_body2d.hpp>
 
 namespace godot {
 
